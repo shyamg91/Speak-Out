8633efc698f9245bb15d8afcc0d72d7fedc461a9
shyamg91 <shyamg7777@gmail.com>
2017-03-26 00:38:25 +1300

    Second layer of paint/Some branding

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.save
-    redirect_to @post
+    redirect_to posts_path
   end
   
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321083847) do
+ActiveRecord::Schema.define(version: 20170325095536) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "message"
-    t.decimal  "votes"
+    t.integer  "votes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
